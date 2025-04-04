6ac21ccfb7f55434808bf89dcb64ba3d0a1fd963
studentbryce <student.bryce@hotmail.com>
2025-04-04 15:19:50 +1300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Nav from './Nav';
+import One from './One';
+import Two from './Two';
+import Three from './Three';
 
 function App() {
   return(
+    <>
     <h1>BareBones react App</h1>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path= "/One" element={<One/>}/>
+        <Route path= "/Two" element={<Two/>}/>
+        <Route path= "/Three" element={<Three/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
